0f1562fa7d7ee2d62c779252a6df98007409ef74
Zahra Ershad <zahraershad.dev@gmail.com>
2026-09-07 16:29:25 +0330

    STYLE: added nav style and added new font

--- Tailwind/FitZone-Tailwind/tailwind.config.js
+++ Tailwind/FitZone-Tailwind/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // نام کلاس: ['نام فونت در گوگل', 'فونت جایگزین']
+        'light': ['Montserrat', 'sans-serif'],   //  300
+        'medium': ['Montserrat', 'sans-serif'],  //  500
+        'black': ['Montserrat', 'sans-serif'],   //  900
+      },
+    },
   },
   plugins: [],
 }
